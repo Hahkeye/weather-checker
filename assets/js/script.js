7a67e1f283bbd6f0874967a8e59415bb28f1c486
Hunter <realisvenator@gmail.com>
2022-09-28 00:11:44 -0500

    Full History implementation and fixed unix time

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,8 +17,9 @@ class Day{
 
     html() {
         let cardDiv = $('<div>');
-        // cardDiv.append($('<h3>').text(this.name));
-        cardDiv.append($('<p>').text(moment(this.date).format("MM/DD/YY")));
+        console.log(this.date);
+        let test = this.date;
+        cardDiv.append($('<p>').text(moment.unix(test).format("MM/DD/YY")));
         cardDiv.append($('<p>').text(`Current Tempature: ${this.currentTempature}°`));
         cardDiv.append($('<p>').text(`Feels Like: ${this.feelsLike}°`));
         cardDiv.append($('<p>').text(`Humidity: ${this.humidity}%`));
@@ -31,14 +32,18 @@ class Day{
 
 function displayHistory(){
     $('.history').empty();
-    console.log(historylist);
+    $('.history').on('click',function(event){
+        if(event.target.nodeName == "BUTTON"){
+            console.log("Target, "+$(event.target).attr('target'));
+            getWeather($(event.target).attr('target'));
+        }
+    });
     for(let i of historylist){
         $('.history').append($('<button>').text(i).attr('target',i));
     }
 }
 
 $(function(){
-    console.log("DASDASDas");
     if(localStorage.getItem('history')){
         historylist=JSON.parse(localStorage.getItem('history'));
         displayHistory();
@@ -46,9 +51,7 @@ $(function(){
 });
 
 function displayDay(day){
-    // console.log(day)
     let today = $('.today-forecast');
-    // today.append(day.html());
     today.children('#name').text(day.name);
     today.children('#date').text(moment().format("MM/DD/YY"))
     today.children('#current-temp').text(`Current Tempature: ${day.currentTempature}°`);
@@ -57,12 +60,15 @@ function displayDay(day){
     today.children('#max-temp').text(`Maxium Tempature: ${day.maxTempature}°`);
     today.children('#min-temp').text(`Minium Tempature: ${day.minTempature}°`);
     today.children('#wind').text(`${day.windSpeed} MPH from ${day.windDirection}°`);
-    get5Day();
+    get5Day(day.name);
+}
+function displayNF(){
+    let today = $('.today-forecast');
+    today.children('#name').text("City Not Found");
 }
 
 function display5Day(data,name){//every 8
     let tempDays = [];
-    // console.log(data.length);
     for(let x=3;x<data.length;x+=8){
         tempDays.push(new Day(data[x].main.temp_min,data[x].main.temp_max,data[x].main.temp,data[x].main.feels_like,data[x].dt,data[x].wind.speed,data[x].wind.deg,data[x].main.humidity,name));
     }
@@ -76,33 +82,38 @@ function display5Day(data,name){//every 8
     }
 }
 
-function get5Day(){
-    let place = $('#cityname').val();
-    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${place}&appid=f850e219e63b844872fab8992ab03f6c`).then(response =>{
+function get5Day(city){
+    if(!city){
+        city=$('#cityname').val();
+    }
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=f850e219e63b844872fab8992ab03f6c`).then(response =>{
         return response.json();
     }).then(data=>{
-        console.log(data);
-        display5Day(data.list,data.city.name);
-    },(error)=>{
-        console.log("DOesnt exists");
+        display5Day(data.list,city);
+    }).catch(function(){
+        displayNF();
+        console.log("Erorr in the api request");
     });
+    // });
 }
 
-function getWeather(){
-    let place = $('#cityname').val();
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=f850e219e63b844872fab8992ab03f6c`).then(response =>{
+function getWeather(city){
+    if(!city){
+        city=$('#cityname').val();
+    }
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=f850e219e63b844872fab8992ab03f6c`).then(response =>{
         return response.json();
     }).then(data=>{
         let tempD = new Day(data.main.temp_min,data.main.temp_max,data.main.temp,data.main.feels_like,data.dt,data.wind.speed,data.wind.deg,data.main.humidity,data.name);
-        console.log(data);
-        if(!historylist.includes(place)){
-            historylist.push(place)
+        if(!historylist.includes(city.toLowerCase())){
+            historylist.push(city.toLowerCase());
             localStorage.setItem("history",JSON.stringify(historylist));
         }
         displayHistory();
         displayDay(tempD);
-    },(error)=>{
-        console.log("DOesnt exists");
+    }).catch(function(){
+        displayNF();
+        console.log("Erorr in the api request");
     });
 }
 
